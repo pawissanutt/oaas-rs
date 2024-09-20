48d53cff4190af013571a4232f58cdeaee397a0c
Pawissanutt Lertpongrujikorn <pawissanutt@gmail.com>
2024-09-20 17:41:50 -0500

    add tools dev-pm

--- tools/src/bin/dev-pm.rs
+++ tools/src/bin/dev-pm.rs
@@ -1,14 +1,23 @@
 use std::{
+    collections::HashMap,
     error::Error,
     net::{IpAddr, Ipv4Addr, SocketAddr},
+    sync::mpsc::Receiver,
 };
 
+use dashmap::DashMap;
 use envconfig::Envconfig;
 use oprc_pb::{
     routing_service_server::{RoutingService, RoutingServiceServer},
-    ClsRouting, ClsRoutingRequest, ClsRoutingTable,
+    ClsRouting, ClsRoutingRequest, ClsRoutingTable, FuncRouting,
+    PartitionRouting,
 };
-use tokio_stream::wrappers::ReceiverStream;
+use tokio::sync::{
+    broadcast, mpsc,
+    watch::{self, channel},
+};
+use tokio_stream::wrappers::{ReceiverStream, WatchStream};
+use tokio_stream::StreamExt;
 use tonic::{transport::Server, Request, Response, Status};
 use tools::create_reflection;
 
@@ -18,7 +27,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let conf = tools::Config::init_from_env()?;
     let socket =
         SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), conf.http_port);
-    let dev_pm = RoutingServiceServer::new(DevPM {});
+    let dev_conf = DevConfig::init_from_env()?;
+    let dev_pm = DevPM::new(dev_conf.get_cls_list());
+    let dev_pm = RoutingServiceServer::new(dev_pm);
     tracing::info!("start server on port {}", conf.http_port);
     let (v1a, v1) = create_reflection();
     Server::builder()
@@ -30,23 +41,104 @@ async fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-struct DevPM {}
+#[derive(envconfig::Envconfig)]
+struct DevConfig {
+    #[envconfig(
+        from = "PM_CLS_LIST",
+        default = "example:echo=localhost:8080|other=localhost:8080"
+    )]
+    cls_list: String,
+}
+
+impl DevConfig {
+    fn get_cls_list(&self) -> DashMap<String, DashMap<String, String>> {
+        let cls_list: Vec<&str> = self.cls_list.split(",").collect();
+        let map: DashMap<String, DashMap<String, String>> = DashMap::new();
+        for cls_expr in cls_list {
+            let cls_fn: Vec<&str> = cls_expr.split(":").collect();
+            let fn_list = cls_fn
+                .get(1)
+                .unwrap_or(&"")
+                .split("|")
+                .map(|s| String::from(s));
+            let fn_map = DashMap::new();
+            for fn_uri in fn_list {
+                let fn_uri: Vec<&str> = fn_uri.split("=").collect();
+                fn_map.insert(
+                    String::from(*fn_uri.get(0).unwrap()),
+                    String::from(*fn_uri.get(1).unwrap()),
+                );
+            }
+            map.insert(String::from(*cls_fn.get(0).unwrap()), fn_map);
+        }
+        map
+    }
+}
+
+struct DevPM {
+    table: ClsRoutingTable,
+    rx: tokio::sync::watch::Receiver<Result<ClsRouting, Status>>,
+    tx: tokio::sync::watch::Sender<Result<ClsRouting, Status>>,
+}
+
+impl DevPM {
+    fn new(cls_map: DashMap<String, DashMap<String, String>>) -> Self {
+        let table = create_table(cls_map);
+        let c = ClsRouting {
+            name: "".into(),
+            partitions: 0,
+            routings: vec![],
+        };
+        let (tx, rx) = watch::channel(Ok(c));
+        Self { rx, tx, table }
+    }
+}
+
+fn create_table(
+    cls_map: DashMap<String, DashMap<String, String>>,
+) -> ClsRoutingTable {
+    let mut cls_routings: Vec<ClsRouting> = Vec::new();
+    for entry in cls_map.iter() {
+        let mut func_routing: HashMap<String, FuncRouting> = HashMap::new();
+        for fn_entry in entry.iter() {
+            func_routing.insert(
+                fn_entry.key().clone(),
+                FuncRouting {
+                    uri: fn_entry.clone(),
+                },
+            );
+        }
+        let partition = PartitionRouting {
+            funcs: func_routing,
+        };
+        let cls = ClsRouting {
+            name: entry.key().clone(),
+            partitions: 1,
+            routings: vec![partition],
+        };
+        cls_routings.push(cls);
+    }
+    let table = ClsRoutingTable { clss: cls_routings };
+    table
+}
 
 #[tonic::async_trait]
 impl RoutingService for DevPM {
     async fn get_cls_routing(
         &self,
-        request: Request<ClsRoutingRequest>,
+        _request: Request<ClsRoutingRequest>,
     ) -> Result<Response<ClsRoutingTable>, tonic::Status> {
-        todo!()
+        Ok(Response::new(self.table.clone()))
     }
 
-    type WatchClsRoutingStream = ReceiverStream<Result<ClsRouting, Status>>;
+    type WatchClsRoutingStream = WatchStream<Result<ClsRouting, Status>>;
 
     async fn watch_cls_routing(
         &self,
-        request: Request<ClsRoutingRequest>,
+        _request: Request<ClsRoutingRequest>,
     ) -> Result<Response<Self::WatchClsRoutingStream>, tonic::Status> {
-        todo!()
+        let rx = self.rx.clone();
+        let rx_stream = WatchStream::from_changes(rx);
+        Ok(Response::new(rx_stream))
     }
 }
